efbf1d6052ae11cecbffc96871af56c23ece42c0
amindeist <amindeist@gmail.com>
2020-03-30 21:29:50 +0430

    lesson 8-Admin controller and routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
+
+
+// Admin Users Routes
+Route::resource('admin/users','AdminUsersController');
